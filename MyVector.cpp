b5ed27bb0560d81289bbab5feffb66b164056c1c
Luke McAlpine <lukedmcalpine@gmail.com>
2024-02-24 21:31:41 -0800

    initial insert and resize

--- MyVector.cpp
+++ MyVector.cpp
@@ -1,29 +1,105 @@
 #include <iostream>
 #include "MyVector.h"
 
-template <class T>
-MyVector<T>::MyVector()
+MyVector::MyVector() : capacity(4), size(0)
 {
-    std::cout << "testing" << std::endl;
+    MyVector::list = new int[capacity];
 }
 
-template <class T>
-MyVector<T>::~MyVector()
+void MyVector::insert(int item, int index)
 {
+
+    if (index < 0)
+    {
+        std::cout << "Index out of bounds" << std::endl;
+        return;
+    }
+    if (MyVector::size == MyVector::capacity)
+    {
+        resize();
+    }
+
+    if (index <= MyVector::size)
+    {
+
+        MyVector::list[index] = item;
+        MyVector::size++;
+        std::cout << "size " << size << "\n";
+        std::cout << "index " << index << "\n";
+        std::cout << "\n";
+    }
+    else
+    {
+        std::cout << "error when size is " << size << " index of " << index << std::endl;
+        return;
+    }
+}
+
+void MyVector::resize()
+{
+    MyVector::capacity = capacity * 2;
+    int *temp = new int[MyVector::capacity];
+
+    // is this necessary?
+    for (size_t i = 0; i < capacity; i++)
+    {
+        temp[i] = 0;
+    }
+
+    // i < size not i < capacity because then it will access memory in list that is not in bounds
+    for (size_t i = 0; i < size; i++)
+    {
+        temp[i] = MyVector::list[i];
+    }
+
+    delete[] MyVector::list;
+    MyVector::list = temp;
+}
+
+void MyVector::print()
+{
+
+    for (int i = 0; i < capacity; i++)
+    {
+        std::cout << MyVector::list[i] << " ";
+    }
+    std::cout << "\n";
+}
+
+MyVector::~MyVector()
+{
+    delete[] MyVector::list;
 }
 
 int main()
 {
-    MyVector<int> myVector = MyVector<int>();
-   // std::cout << &myVector.list << std::endl;
-    // std::vector<int> v = {8,4,5,9};
 
-    // v.push_back(6);
-    // v.push_back(9);
+    // my process with insert was to first just get one item inserted
+    // then it was to try adding elements out of range
+    // this worked initially but it was evaluating an unsigned integer vs signed integer which was causing size to be incremented even though nothing was added (in what I could see with my print function)
+    // then when I changed size to be int and not size_t it was causing the index -1 to pass the check of if index < size. So size was being incremented.
+    // this caused a bug where the size was being counted as one more than how many elements were actually in the array
+    // so it was making the size one more then it should have been
+    // it was causing a "double free or corruption (out)" and "Aborted (core dumped)"
+    // then i was having an issue where I was doing i < capacity but it was accessing elements not part of the list array
+    //  // i < size not i < capacity because then it will access memory in list that is not in bounds
+    // that is why when I was printing list that it had random values that I didnt add.
 
-    // v[2] = -1;
+    MyVector myVector = MyVector();
+    // myVector.insert(1, -1);
+    // myVector.insert(4, 15);
 
-    // for (int n: v)
-    //     std::cout<< n << ' ';
-    // std::cout << '\n';
+    // myVector.insert(17, 17);
+    for (int i = 0; i < 4; i++)
+    {
+        myVector.insert(i + 1, i);
+    }
+    myVector.print();
+    myVector.insert(1, -1);
+    myVector.print();
+    myVector.insert(5, 4);
+    myVector.print();
+    myVector.insert(17, 17);
+
+    myVector.print();
 }
--- MyVector.h
+++ MyVector.h
@@ -1,12 +1,22 @@
-template <class T>
+//template <class T>
 class MyVector
 {
 private:
-    T *list;
-    size_t size;
+    int *list;
+
+    // number of elements in the list
+    int size;
+    // total number of elements the array can hold
     int capacity;
 
 public:
     MyVector();
     ~MyVector();
+
+    void insert(int item, int index);
+
+void resize();
+
+    void print();
+    
 };
